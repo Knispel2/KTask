9c53675a20d26eee58f4bf42e8a262365d3528e9
Timofey Pshanichniy <timofei.psheno@gmail.com>
2022-04-09 21:49:20 +0300

    Реализация основных функций

--- KTask.cpp
+++ KTask.cpp
@@ -28,13 +28,28 @@ vector <string> list_files(string dir)
     return result;
 }
 
-Cord split(string data, string file_debug = "")
+Cord split(string & data, string file_debug = "")
 {
     auto pos = data.find(" ");
     int transp;
     if (data.find("  ") != string::npos) transp = 2;
     else transp = 1;
     return Cord(stod(data.substr(0, pos)), stod(data.substr(pos + transp)));
+}
+
+bool check_perm(vector <unsigned int> & base)
+{
+    for (int i = 0; i < base.size(); i++)
+        if (base[base[i] - 1] == i + 1) return false;
+    return true;
+}
+
+double cost_perm(vector <unsigned int>& permutations, vector <Cord>& cords)
+{
+    double result = 0;
+    for (int i = 0; i < permutations.size(); i++)
+        result += dist(cords[i+1], cords[permutations[i]-1]);
+    return result;
 }
 
 int main()
@@ -55,6 +70,16 @@ int main()
             cords.push_back(split(buf, x));
         }
         file.close();
+        vector <unsigned int> permutations(cords.size());
+        for (int i = 0; i < cords.size(); i++)
+            permutations[0] = i + 1;
+        double buf;
+        do {
+            if (check_perm(permutations))
+            {
+                buf = cost_perm(permutations, cords);
+            }
+        } while (next_permutation(permutations.begin(), permutations.end()));
 
 
         fout << x << ":" << result << endl;      
